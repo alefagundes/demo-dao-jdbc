3023dcfb882f189d6d09c5935d862d5f532fa92c
alefagundes <alefagundes2000.af@gmail.com>
2021-11-11 07:12:57 -0300

    Seller entity class

--- src/apllication/Program.java
+++ src/apllication/Program.java
@@ -5,8 +5,9 @@ import model.entities.Department;
 public class Program {
 
 	public static void main(String[] args) {
-		
+	
 		Department obj = new Department(1, "books");
+		
 		System.out.println(obj);
 	}
 
