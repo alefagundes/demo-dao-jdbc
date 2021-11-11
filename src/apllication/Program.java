7efb843a00b98797a31108fd05fc4dba55b81531
alefagundes <alefagundes2000.af@gmail.com>
2021-11-11 07:35:45 -0300

    SellerDaoJDBC and DaoSeller

--- src/apllication/Program.java
+++ src/apllication/Program.java
@@ -1,14 +1,20 @@
 package apllication;
 
+import java.util.Date;
+
+import model.dao.DaoFactoty;
+import model.dao.SellerDao;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Date hj = new Date();
 		
 		Department obj = new Department(1, "books");
 		
-		System.out.println(obj);
+		SellerDao sellerdao = DaoFactoty.createSellerDao();
 	}
 
 }
