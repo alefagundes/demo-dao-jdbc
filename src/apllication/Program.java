baaca47493e404a54b03ff62f308ae86a6d2a558
alefagundes <alefagundes2000.af@gmail.com>
2021-11-11 07:22:35 -0300

    DepartmentDao and SellerDao interfaces

--- src/apllication/Program.java
+++ src/apllication/Program.java
@@ -5,7 +5,7 @@ import model.entities.Department;
 public class Program {
 
 	public static void main(String[] args) {
-	
+		
 		Department obj = new Department(1, "books");
 		
 		System.out.println(obj);
