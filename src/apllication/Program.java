23c5a8f8c3af6f30d6b5da8a1f7eefc24e1db243
alefagundes <alefagundes2000.af@gmail.com>
2021-11-13 13:18:15 -0300

    Update implementation

--- src/apllication/Program.java
+++ src/apllication/Program.java
@@ -1,5 +1,9 @@
 package apllication;
 
+
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import model.dao.DaoFactoty;
@@ -11,6 +15,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		
 		SellerDao sellerDao = DaoFactoty.createSellerDao();
 		
 		System.out.println("=== Test 1: seller findDyId =====");
@@ -19,7 +24,7 @@ public class Program {
 	
 		System.out.println(seller);
 		
-		System.out.println("\n=== Test 1: seller findDyId =====");
+		System.out.println("\n=== Test 2: seller findDyId =====");
 		
 		Department department = new Department(2, null);
 		
@@ -27,11 +32,21 @@ public class Program {
 		for(Seller s : list) {
 			System.out.println(s);
 		}
-		System.out.println("\n=== Test 1: seller findAll =====");
+		System.out.println("\n=== Test 3: seller findAll =====");
 		
 		list = sellerDao.findAll();
 		for(Seller s : list) {
 			System.out.println(s);
 		}
+		System.out.println("\n=== Test 4: seller insert =====");
+		Seller newSeller = new Seller(null, "greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== Test 5: seller update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 	}
 }
