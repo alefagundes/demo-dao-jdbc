4000b6a659f6cd2902fdca4b03d1ae8b416d1b9d
alefagundes <alefagundes2000.af@gmail.com>
2021-11-15 13:37:57 -0300

    Delete and Update methods implementation

--- src/apllication/Program2.java
+++ src/apllication/Program2.java
@@ -10,10 +10,11 @@ import model.entities.Department;
 public class Program2 {
 
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
 		
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
-		
+
 		System.out.println("=== TEST 1: findById =======");
 		Department dep = departmentDao.findById(1);
 		System.out.println(dep);
@@ -23,14 +24,24 @@ public class Program2 {
 		for (Department d : list) {
 			System.out.println(d);
 		}
-		
+
 		System.out.println("\n=== TEST 3: insert =======");
 		Department newDepartment = new Department(null, "Music");
 		departmentDao.insert(newDepartment);
 		System.out.println("Inserted! New id: " + newDepartment.getId());
+
+		System.out.println("\n=== TEST 4: update =======");
+		Department dep2 = departmentDao.findById(3);
+		dep2.setName("Food");
+		departmentDao.update(dep2);
+		System.out.println("Update completed");
 		
-		
-		
+		System.out.println("\n=== TEST 5: delete =======");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed");
+
 		sc.close();
 	}
 
